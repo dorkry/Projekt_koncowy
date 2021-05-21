0e9dabeec2b9e87272fdbddd6da535da49830307
Dorota Krygowska <dorota.krygowska@gmail.com>
2021-05-21 22:42:36 +0200

    login&password

--- blog/routes.py
+++ blog/routes.py
@@ -1,10 +1,10 @@
 # blog/routes.py
 
 from faker import Faker
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
 
 @app.route("/")
 def index():
@@ -42,7 +42,30 @@ def handle_entry(method, form, entry):
            errors = form.errors
     return render_template("entry_form.html", form = form, errors = errors)
 
+from flask import render_template, request, session, flash, redirect, url_for
+from blog.forms import LoginForm
 
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
 
 def generate_entries(how_many=10):
    fake = Faker()
